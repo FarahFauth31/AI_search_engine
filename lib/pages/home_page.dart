db44e08950df950b1ccda2497f544c96fae82c41
Farah <fafauth31@gmail.com>
2025-04-09 19:12:23 +0200

    added side bar

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:perplexity_clone/widgets/side_bar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SideBar(),
+          Column(
+
+          ),
+        ],
+      ),
+    );
+  }
+}
